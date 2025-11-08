12c35556ca89ed6b93372a7d14585d1f5d101135
MikhailNagibin <nagibimi@gmail.com>
2025-11-09 01:11:17 +0300

    add getByIndex for Notification

--- src/main/java/com/coactivity/domain/Notification.java
+++ src/main/java/com/coactivity/domain/Notification.java
@@ -4,5 +4,13 @@ public enum Notification {
   membershipAccepted,
   membershipRejected,
   activityClosed,
-  newJoinRequest
+  newJoinRequest;
+
+  public static Notification getByIndex(int index) {
+    Notification[] notification = values();
+    if (index >= 0 && index < notification.length) {
+      return notification[index];
+    }
+    throw new IllegalArgumentException("Invalid role notification: " + index);
+  }
 }
