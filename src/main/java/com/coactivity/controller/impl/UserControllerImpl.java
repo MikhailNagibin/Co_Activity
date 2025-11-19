958546e3821d20d524609017c16177945ae1e548
BumaginNikita <bumagin.ns@phystech.edu>
2025-11-20 00:39:16 +0300

    dump

--- src/main/java/com/coactivity/controller/impl/UserControllerImpl.java
+++ src/main/java/com/coactivity/controller/impl/UserControllerImpl.java
@@ -1,11 +1,5 @@
 package com.coactivity.controller.impl;
 
-import com.coactivity.service.UserService;
-import com.coactivity.controller.dto.response.ApiResponse;
-import com.coactivity.controller.dto.response.UserProfileResponse;
-
 public class UserControllerImpl {
-  public ApiResponse<UserProfileResponse> getUserProfile(int token) {
-    return UserService.getUserProfile(token);
-  }
+
 }
