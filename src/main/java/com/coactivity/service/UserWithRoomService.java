01ff53adbc6ad1961e6efdd36fc5854c78ad7722
MikhailNagibin <nagibimi@gmail.com>
2025-11-20 17:39:57 +0300

    add methods

--- src/main/java/com/coactivity/service/UserWithRoomService.java
+++ src/main/java/com/coactivity/service/UserWithRoomService.java
@@ -1,8 +1,60 @@
 package com.coactivity.service;
 
+import com.coactivity.DataRepository;
+import com.coactivity.controller.dto.response.ApiResponse;
+import com.coactivity.domain.Role;
+import com.coactivity.repository.impl.RoomRepositoryImpl;
+import com.coactivity.repository.impl.UserRepositoryImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserWithRoomService {
+  private final UserRepositoryImpl userRepository;
+  private final DataRepository dataRepository;
+  private final RoomRepositoryImpl roomRepository;
+  private final TokenService tokenService;
 
+  public UserWithRoomService(DataRepository repository,
+                            UserRepositoryImpl userRepository,
+                            RoomRepositoryImpl roomRepository,
+                            TokenService tokenService) {
+    this.dataRepository = repository;
+    this.userRepository = userRepository;
+    this.roomRepository = roomRepository;
+    this.tokenService = tokenService;
+  }
+
+  public ApiResponse<Void> assignAdminRole(String token, Integer roomId,
+                                           Integer userId) {
+
+    Integer roomOwnerId = tokenService.decodeToken(token).userId();
+    try {
+      if (!roomRepository.isUserOwnerOfRoom(roomOwnerId, roomId)) {
+        return ApiResponse.error(null);
+      }
+
+      roomRepository.setRoleByUserIdAndRoomId(userId, roomId, Role.ADMIN);
+      return ApiResponse.success(null);
+
+    } catch (Exception e) {
+      return ApiResponse.error("400");
+    }
+  }
+
+  public ApiResponse<Void> demoteAdminRole(String token, Integer roomId,
+                                           Integer userId) {
+
+    Integer roomOwnerId = tokenService.decodeToken(token).userId();
+    try {
+      if (!roomRepository.isUserOwnerOfRoom(roomOwnerId, roomId)) {
+        return ApiResponse.error(null);
+      }
+
+      roomRepository.setRoleByUserIdAndRoomId(userId, roomId, Role.PARTICIPANT);
+      return ApiResponse.success(null);
+
+    } catch (Exception e) {
+      return ApiResponse.error("400");
+    }
+  }
 }
